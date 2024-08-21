58c1b98fd8a77470e7d1eea060bce12ee3ac4986
trevor <trevor@theleybournes.com>
2024-08-21 12:58:36 +1200

    Added new components, removed some duplicates, added a Branchline line to each component so we can control the branch line and also make it dashed.

--- DCCPanelController/Components/TrackImages/TrackImages.cs
+++ DCCPanelController/Components/TrackImages/TrackImages.cs
@@ -31,43 +31,41 @@ public static class TrackImages {
     /// that represents the in and out paths that the track can support. 
     /// </summary>
     private static void AddTrackImages() {
-        Add("*S***S**","Track Angle","Track_Angle");
-        Add("*S***S**","Track Angle with Button","Track_Angle_Button");
-        Add("*S***S**","Track Angle with Button (Short Side)","Track_Angle_Button_Short");
-        Add("*T***S**","Track Angle - Terminated (Long)","Track_Angle_Terminator_Long");
-        Add("*T***S**","Track Angle - Terminated (Short)","Track_Angle_Terminator_Short");
-        Add("*T***S**","Track Angle - Terminated (Lines)","Track_Angle_Terminator_Short_Lines");
-        Add("**C***S*","Track Page Continuation (Arrow)","Track_Continuation_Arrow");
-        Add("**C***S*","Track Page Continuation (Lines)","Track_Continuation_Lines");
-        Add("*S****S*","Track Corner (Left)","Track_Corner_Left");
-        Add("*S****S*","Track Corner (Left) with Button","Track_Corner_Left_Button");
-        Add("*S****S*","Track Corner (Left) with Button (Long Side)","Track_Corner_Left_Button_Long");
-        Add("*S****S*","Track Corner (Left) with Button (Short Side)","Track_Corner_Left_Button_Short");
-        Add("*C****S*","Track Corner (Left) Page Continuation (Arrow)","Track_Corner_Left_Continuation_Arrow");
-        Add("*C****S*","Track Corner (Left) Page Continuation (Lines)","Track_Corner_Left_Continuation_Lines");
-        Add("***S**S","Track Corner (Right)","Track_Corner_Right");
-        Add("***S**S","Track Corner (Right) with Button","Track_Corner_Right_Button");
-        Add("***S**S","Track Corner (Right) with Button (Long Side)","Track_Corner_Right_Button_Long");
-        Add("***S**S","Track Corner (Right) with Button (Short Side)","Track_Corner_Right_Button_Short");
-        Add("***C**S","Track Corner (Right) Page Continuation (Arrow)","Track_Corner_Right_Continuation_Arrow");
-        Add("***C**S","Track Corner (Right) Page Continuation (Lines)","Track_Corner_Right_Continuation_Lines");
-        Add("*S*S*S*S","Track Crossing (Angle)","Track_Crossing_Angle");
-        Add("S*S*S*S*","Track Crossing (Straight)","Track_Crossing_Straight");
-        Add("**S***S*","Track Straight","Track_Straight");
-        Add("**S***S*","Track Straight with Button","Track_Straight_Button");
-        Add("**T***S*","Track Terminator","Track_Terminator");
-        Add("*SSS**S*","Track Threeway","Track_Threeway");
-        Add("*DXX**S*","Track Threeway (Left)","Track_Threeway_Left");
-        Add("*XXD**S*","Track Threeway (Right)","Track_Threeway_Right");
-        Add("*XSX**S*","Track Threeway (Straight)","Track_Threeway_Straight");
-        Add("*DS***S*","Track Turnout (Left)","Track_Turnout_Left");
-        Add("*DX***S*","Track Turnout (Left) Diverging","Track_Turnout_Left_Diverging");
-        Add("*DS***S*","Track Turnout (Left) With Label","Track_Turnout_Left_Label");
-        Add("*XS***S*","Track Turnout (Left) Straight","Track_Turnout_Left_Straight");
-        Add("**SD**S*","Track Turnout (Right)","Track_Turnout_Right");
-        Add("**XD**S*","Track Turnout (Right) Diverging","Track_Turnout_Right_Diverging");
-        Add("**SD**S*","Track Turnout (Right) With Label","Track_Turnout_Right_Label");
-        Add("**SX**S*","Track Turnout (Right) Straight","Track_Turnout_Right_Straight");
+        Add("********","Button"                                         ,"Track_Button");
+        Add("********","Label"                                          ,"Track_Label");
+        
+        Add("**S***S*","Track (Straight)"                               ,"Track_Straight");
+        Add("**C***S*","Track Page Continuation Arrow (Straight)"       ,"Track_Straight_Continuation_Arrow");
+        Add("**C***S*","Track Page Continuation Lines (Straight)"       ,"Track_Straight_Continuation_Lines");
+        Add("S*S*S*S*","Track Crossing (Straight)"                      ,"Track_Straight_Cross");
+        Add("**T***S*","Track Terminator (Straight)"                    ,"Track_Straight_Terminator");
+       
+        Add("*S***S**","Track (Angle)"                                  ,"Track_Angle");
+        Add("*C***S**","Track Page Continuation Arrow (Angle)"          ,"Track_Angle_Continuation_Arrow");
+        Add("*C***S**","Track Page Continuation Lines (Angle)"          ,"Track_Angle_Continuation_Lines");
+        Add("*S*S*S*S","Track Crossing (Angle)"                         ,"Track_Angle_Cross");
+        Add("*T***S**","Track Terminator (Angle)"                       ,"Track_Angle_Terminator");
+        
+        Add("*S****S*","Track Corner (Left)"                            ,"Track_Corner_Left");
+        Add("*C****S*","Track Corner Page Continuation Arrow (Left)"    ,"Track_Corner_Left_Continuation_Arrow");
+        Add("*C****S*","Track Corner Page Continuation Lines (Left)"    ,"Track_Corner_Left_Continuation_Lines");
+
+        Add("***S**S*","Track Corner (Right)","Track_Corner_Right");
+        Add("***C**S*","Track Corner Page Continuation Arrow (Right)"   ,"Track_Corner_Right_Continuation_Arrow");
+        Add("***C**S*","Track Corner Page Continuation Lines (Right)"   ,"Track_Corner_Right_Continuation_Lines");
+        
+        Add("*SSS**S*","Track Threeway"                                 ,"Track_Threeway");
+        Add("*DXX**S*","Track Threeway (Left)"                          ,"Track_Threeway_Left");
+        Add("*XXD**S*","Track Threeway (Right)"                         ,"Track_Threeway_Right");
+        Add("*XSX**S*","Track Threeway (Straight)"                      ,"Track_Threeway_Straight");
+
+        Add("*DS***S*","Track Turnout (Left)"                           ,"Track_Turnout_Left");
+        Add("*DX***S*","Track Turnout (Left) Diverging"                 ,"Track_Turnout_Left_Diverging");
+        Add("*XS***S*","Track Turnout (Left) Straight"                  ,"Track_Turnout_Left_Straight");
+
+        Add("**SD**S*","Track Turnout (Right)"                          ,"Track_Turnout_Right");
+        Add("**XD**S*","Track Turnout (Right) Diverging"                ,"Track_Turnout_Right_Diverging");
+        Add("**SX**S*","Track Turnout (Right) Straight"                 ,"Track_Turnout_Right_Straight");
     }
 
     private static void Add(string directions, string name, string reference) {
