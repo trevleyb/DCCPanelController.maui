e7c6e3924fe2b732b42fcb319e56ee4fae605aeb
Trevor Leybourne <trevor@theleybournes.com>
2025-10-19 19:44:38 +1300

    Changed the entity name to match the track type

--- DCCPanelController/Models/DataModel/Entities/StraightEntity.cs
+++ DCCPanelController/Models/DataModel/Entities/StraightEntity.cs
@@ -32,11 +32,44 @@ public partial class StraightEntity : TrackEntity, ITrackEntity {
             TrackStyleEnum.Rounded    => EntityConnections.TrackPatterns.TerminatorTrack,
             _                         => EntityConnections.TrackPatterns.StraightTrack,
         };
-    
-    [JsonIgnore] public override string EntityName => "Track";
-    [JsonIgnore] public override string EntityDescription => "Straight Track";
+
+    [JsonIgnore] public override string EntityName =>
+        TrackStyle switch {
+            TrackStyleEnum.Normal     => "Straight",
+            TrackStyleEnum.Tunnel     => "Tunnel",
+            TrackStyleEnum.Bridge     => "Bridge",
+            TrackStyleEnum.Platform   => "Platform",
+            TrackStyleEnum.Terminator => "Terminator",
+            TrackStyleEnum.Lines      => "Lines",
+            TrackStyleEnum.Arrow      => "Arrow",
+            TrackStyleEnum.Rounded    => "Rounded",
+            _                         => "Track"
+        };
+
+    [JsonIgnore] public override string EntityDescription => TrackStyle switch {
+        TrackStyleEnum.Normal     => "Straight Track",
+        TrackStyleEnum.Tunnel     => "Tunnel Entrance",
+        TrackStyleEnum.Bridge     => "Bridge Side Track",
+        TrackStyleEnum.Platform   => "Platform Track",
+        TrackStyleEnum.Terminator => "Terminator End",
+        TrackStyleEnum.Lines      => "Lines Continuation",
+        TrackStyleEnum.Arrow      => "Arrow Continuation",
+        TrackStyleEnum.Rounded    => "Rounded End",
+        _                         => "Normal Track"
+    };
+
     [JsonIgnore] public override string EntityInformation =>
-        "The **straight** is a straight piece of track. ";
+        TrackStyle switch {
+            TrackStyleEnum.Normal     => "The **straight** is a straight piece of track. ",
+            TrackStyleEnum.Tunnel     => "The **tunnel** represents the entrance or exit of a tunnel. Use it with Dashed Track.",
+            TrackStyleEnum.Bridge     => "The **bridge track** shows an indicator to represent that the track is over a bridge.",
+            TrackStyleEnum.Platform   => "The **platform** shows an indicator, either on top or bottom, to represent a platform at a station. ",
+            TrackStyleEnum.Terminator => "The **terminator**  (or **buffer stop**) is used to indicate the end of a track. ",
+            TrackStyleEnum.Lines      => "The **lines continuation** indicates that the track continues on another panel or page.",
+            TrackStyleEnum.Arrow      => "The **arrow continuation** indicates that the track continues on another panel or page.",
+            TrackStyleEnum.Rounded    => "The **rounded track** is a rounded end to the track.",
+            _                         => "A **normal track** is a straight piece of track. ",
+        };
 
     [JsonIgnore] protected override int RotationFactor => 45;
 
