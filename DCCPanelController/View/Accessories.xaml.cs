517a838c81209cc9d62270e2bf885cc00da6cd5a
Trevor Leybourne <trevor@theleybournes.com>
2025-12-31 09:49:24 +1300

    Separated the Tables (Accessories) from the Panel Editor page and added a new page which will explain the Accessories.

---
+++ DCCPanelController/View/Accessories.xaml.cs
@@ -0,0 +1,12 @@
+using DCCPanelController.Helpers;
+using DCCPanelController.View.Helpers;
+using Syncfusion.Maui.Toolkit.Popup;
+
+namespace DCCPanelController.View;
+
+public partial class AccessoriesPage : ContentPage {
+    public AccessoriesPage() {
+        BindingContext = this;
+        InitializeComponent();
+    }
+}
