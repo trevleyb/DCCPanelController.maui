fb41750891632ff05a9276ea39fea60afe254d4e
Trevor Leybourne <trevor@theleybournes.com>
2025-12-31 10:35:18 +1300

    Added Accessories Help page

--- DCCPanelController/View/Accessories.xaml.cs
+++ DCCPanelController/View/Accessories.xaml.cs
@@ -9,4 +9,20 @@ public partial class AccessoriesPage : ContentPage {
         BindingContext = this;
         InitializeComponent();
     }
+
+    private async void OnTurnoutsClicked(object? sender, EventArgs e) {
+        await Shell.Current.GoToAsync("//Accessories/Turnouts");
+    }
+
+    private async void OnRoutesClicked(object? sender, EventArgs e) {
+        await Shell.Current.GoToAsync("//Accessories/Routes");
+    }
+
+    private async void OnBlocksClicked(object? sender, EventArgs e) {
+        await Shell.Current.GoToAsync("//Accessories/Blocks");
+    }
+
+    private async void OnLightsClicked(object? sender, EventArgs e) {
+        await Shell.Current.GoToAsync("//Accessories/Lights");
+    }
 }
